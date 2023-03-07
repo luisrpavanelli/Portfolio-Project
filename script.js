958864f0870b9d64979e9f3d31d0b82521004876
Luis Renan Pavanello <102007776+luisrpavanelli@users.noreply.github.com>
2023-03-06 22:04:36 -0300

    Add modifies in HTML, CSS, license, and Linter Files

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('menu-links')[0];
 const dimBackground = document.getElementsByClassName('dim-background')[0];
 const exitMobileButton = document.getElementsByClassName('exit-mobile-menu')[0];
+const menuTabs = document.getElementsByClassName('menutabs');
 
 toggleButton.addEventListener('click', () => {
-     navBarLinks.classList.toggle('active');
-     dimBackground.classList.toggle('active');
-
-     toggleButton.classList.toggle('active');
-     exitMobileButton.classList.toggle('active');
+  navBarLinks.classList.toggle('active');
+  dimBackground.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  exitMobileButton.classList.toggle('active');
 });
-
 exitMobileButton.addEventListener('click', () => {
-     navBarLinks.classList.toggle('active');
-     dimBackground.classList.toggle('active');
-     toggleButton.classList.toggle('active');
-     exitMobileButton.classList.toggle('active');
+  navBarLinks.classList.toggle('active');
+  dimBackground.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  exitMobileButton.classList.toggle('active');
 });
+
+for (var i = 0; i < menuTabs.length; i++) {
+  menuTabs[i].addEventListener('click', () => {
+    navBarLinks.classList.toggle('active');
+    dimBackground.classList.toggle('active');
+    toggleButton.classList.toggle('active');
+    exitMobileButton.classList.toggle('active');
+  });
+}
